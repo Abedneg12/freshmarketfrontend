9efc50fee7aed215d7fbef438dc435caa5292fdb
Abedneg12 <yosua12.abednego@gmail.com>
2025-08-02 00:34:37 +0700

    rename

--- src/app/super-admin/market/page.tsx
+++ src/app/super-admin/market/page.tsx
@@ -1,7 +1,7 @@
 import StoreManagementPage from "@/pages/Store-Management-page";
 import { Suspense } from "react";
 
-export default function Stores() {
+export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <StoreManagementPage />
