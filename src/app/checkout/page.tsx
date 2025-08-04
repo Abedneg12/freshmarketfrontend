6bf3431544679c9fb3f629ec822c868eb92c2c85
irga diputra <irgajuliand@gmail.com>
2025-08-04 23:15:22 +0700

    add auth protection page to checkout

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,9 +1,10 @@
 'use client';
-import CheckoutPage from '@/pages/Checkout-page';
+import CheckoutPage from '@/pages/checkout-page';
 import React from 'react'
+import withAuth from '@/components/common/Auth';
 
-export default function page() {
-  return (
-    <CheckoutPage />
-  )
-}
+const Page = () => {
+  return <CheckoutPage />;
+};
+
+export default withAuth(Page);
